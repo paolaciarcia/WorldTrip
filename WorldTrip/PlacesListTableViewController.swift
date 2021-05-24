f44d016557518d4dd0f091426d69587aedb4f04c
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-05-24 12:01:21 -0300

    add PlacesListTableViewController

---
+++ WorldTrip/PlacesListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlacesListTableViewController.swift
+//  WorldTrip
+//
+//  Created by Erick Borges on 24/05/21.
+//
+
+import UIKit
+
+class PlacesListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
