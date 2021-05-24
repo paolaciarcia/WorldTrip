2c35ea3f97e94b0b2620b3f4b02366bbf5576250
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-05-24 19:13:16 -0300

    add CLGeocoder

--- WorldTrip/Controllers/PlaceFinderViewController.swift
+++ WorldTrip/Controllers/PlaceFinderViewController.swift
@@ -17,15 +17,40 @@ class PlaceFinderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func chooseButton(_ sender: UIButton) {
+        searchTextField.resignFirstResponder()
+        
+        if searchTextField.text != nil {
+            let place = searchTextField.text!
+            load(show: true)
+            let cLGeocoder = CLGeocoder()
+        
+            cLGeocoder.geocodeAddressString(place) { (placemarks, error) in
+                self.load(show: false)
+                
+                guard let placeMark = placemarks?.first else { return }
+                print("place: \(place)  placemarks: \(String(describing: placemarks?.first))")
+                print(Place.getFormattedAddress(with: placeMark))
+            }
+        } 
     }
+    
     
     @IBAction func closeButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func load(show: Bool) {
+        viewLoading.isHidden = !show
+        if show {
+            loading.startAnimating()
+        } else {
+            loading.stopAnimating()
+        }
     }
     
 }
---
+++ WorldTrip/Models/Place.swift
@@ -0,0 +1,48 @@
+//
+//  Place.swift
+//  WorldTrip
+//
+//  Created by Erick Borges on 24/05/21.
+//
+
+import Foundation
+import MapKit
+
+
+struct Place {
+    let name: String
+    let lat: CLLocationDegrees
+    let long: CLLocationDegrees
+    let address: String
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+    
+    static func getFormattedAddress(with placemark: CLPlacemark) -> String {
+        var address = ""
+        
+        if let street = placemark.thoroughfare {
+            address += street
+        }
+        if let number = placemark.subThoroughfare {
+            address += " \(number)"
+        }
+        if let subLocality = placemark.subLocality {
+            address += ", \(subLocality)" //Bairro
+        }
+        if let city = placemark.locality {
+            address += "\n\(city)"
+        }
+        if let state = placemark.administrativeArea {
+            address += " - \(state)"
+        }
+        if let postalCode = placemark.postalCode {
+            address += "\nCEP:\(postalCode)"
+        }
+        if let county = placemark.country {
+            address += "\n\(county)"
+        }
+        return address
+    }
+}
