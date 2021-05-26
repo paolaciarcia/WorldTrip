a60d8af66aa83066fa5c7e7c7a7102bff47ef4c8
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-05-26 11:43:14 -0300

    add annotations on map using MKAnnotation

--- WorldTrip/Controllers/MapViewController.swift
+++ WorldTrip/Controllers/MapViewController.swift
@@ -10,15 +10,32 @@ import MapKit
 
 class MapViewController: UIViewController {
     
+    //todos os lugares a serem visualizados
+    var allPlaces: [Place]!
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var placeName: UILabel!
     @IBOutlet weak var addressName: UILabel!
+    @IBOutlet weak var viewInfo: UIView!
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.isHidden = true
+        viewInfo.isHidden = true
+        
+        if allPlaces.count == 1 {
+            title = allPlaces[0].name
+        } else {
+            title = "Meus lugares"
+        }
+        
+        
+        for place in allPlaces {
+            addPlacesToMap(place)
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +43,21 @@ class MapViewController: UIViewController {
     @IBAction func plotRoute(_ sender: UIButton) {
     }
     @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+    }
+    
+    //implementacao do MKAnnotation - "pinos" que são inseridos no mapa
+    func addPlacesToMap(_ place: Place) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = place.coordinate
+        annotation.title = place.name
+        //annotation.subtitle = place.address
+        mapView.addAnnotation(annotation)
+        
+        //mostrar todas as anotacoes que o mapa tem, mesmo que estejam distantes
+        showPlaces()
+    }
+    
+    func showPlaces() {
+        mapView.showAnnotations(mapView.annotations, animated: true)
     }
 }
--- WorldTrip/Controllers/PlacesListTableViewController.swift
+++ WorldTrip/Controllers/PlacesListTableViewController.swift
@@ -11,17 +11,38 @@ class PlacesListTableViewController: UITableViewController {
     
     var places: [Place] = []
     let ud = UserDefaults.standard
+    //label de inicio quando não há nenhum lugar salvo pelo usuário
+    var startedLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadPlaces()
-        savePlaces()
+        setupLabel()
+        //savePlaces()
+    }
+    
+    func setupLabel() {
+        startedLabel = UILabel()
+        startedLabel.text = "Adicione os lugares que deseja conhecer\nclicando no botão + acima"
+        startedLabel.textAlignment = .center
+        startedLabel.numberOfLines = 0
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier! != "showPlace" {
             let mapVC = segue.destination as! PlaceFinderViewController
             mapVC.delegate = self
+        } else {
+            //informações apresentadas para a próxima tela
+            //caso o usuário escolha um lugar específico, será mostradas infos especificas do lugar ecolhido. Caso ele nao escolha um lugar especifico, todos os lugares serão mostrados no mapa pelo botão Mostrar lugares no mapa
+            let vc = segue.destination as! MapViewController
+            
+            switch sender {
+            case let place as Place:
+                vc.allPlaces = [place]
+            default:
+                vc.allPlaces = places
+            }
         }
     }
     
@@ -42,7 +63,6 @@ class PlacesListTableViewController: UITableViewController {
         ud.set(json, forKey: "places")
     }
     
-    
 
     // MARK: - Table view data source
 
@@ -51,6 +71,17 @@ class PlacesListTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //mostrar o botão caso o número de lugares selecionados seja maior que 0
+        if places.count > 0 {
+            let showPlacesButton = UIBarButtonItem(title: "Mostrar lugares no mapa", style: .plain, target: self, action: #selector(showAll))
+            navigationItem.leftBarButtonItem = showPlacesButton
+            tableView.backgroundView = nil
+        } else {
+            navigationItem.leftBarButtonItem = nil
+            tableView.backgroundView = startedLabel
+        }
+        
+        
         return places.count
     }
     
@@ -60,6 +91,26 @@ class PlacesListTableViewController: UITableViewController {
         cell.textLabel?.text = place.name
         
         return cell
+    }
+    
+    //marcar no mapa o lugar que usuário selecionou
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let place = places[indexPath.row]
+        performSegue(withIdentifier: "showPlace", sender: place)
+    }
+    
+    //mostrar todos os lugares selecionados no mapa
+    @objc func showAll() {
+        performSegue(withIdentifier: "showPlace", sender: nil)
+    }
+    
+    //permite remover celulas existentes na tableView
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            places.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            savePlaces()
+        }
     }
 }
 
